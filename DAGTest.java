f0c2d083e6b43a41ea7f6d6fe93ab6858b669dd0
cpower97 <35492377+cpower97@users.noreply.github.com>
2018-10-18 20:00:24 +0100

    Additional Test coverage

--- DAGTest.java
+++ DAGTest.java
@@ -19,4 +19,61 @@ public class DAGTest {
 		DAG graph = new DAG(-1);
 	}
 
+	//Test the DAG constructor
+	@Test
+	public void testGraphConstructor()
+	{
+		DAG graph = new DAG(0);
+		assertEquals("This graph has no vertices", 0, graph.V());
+		assertEquals("This graph has no edges", 0, graph.E());
+		assertNull("The adjacency list is empty",graph.adj(0));
+		
+		
+		graph = new DAG(1);
+		assertEquals("This graph has 1 vertex", 1, graph.V());
+		assertEquals("This graph has no edges", 0, graph.E());
+		assertNotNull("The adjacency list is not empty", graph.adj(0));
+		
+		graph = new DAG(1000);
+		assertEquals("This graph has 1000 vertices", 1000, graph.V());
+		assertEquals("This graph has no edges", 0, graph.E());
+		assertNotNull("The adjacency list is not empty", graph.adj(999));
+		
+	}
+	
+	//Test isEmpty function
+	@Test
+	public void testIsEmpty()
+	{
+		DAG graph = new DAG(0);
+		assertTrue("This graph is empty", graph.isEmpty());
+		
+		graph = new DAG(1);
+		assertFalse("This graph is not empty",graph.isEmpty());
+	}
+	
+	//Tests validateVertex function
+	@Test
+	public void testValidateVertex()
+	{
+		DAG graph = new DAG(10);
+		assertEquals(-1, graph.validateVertex(-1));
+		assertEquals(-1, graph.validateVertex(11));
+		assertEquals(1, graph.validateVertex(9));
+	}
+	
+	//Test indegree function
+	@Test
+	public void testIndegree()
+	{
+		
+	}
+
+	//Tests outdegree function
+	@Test
+	public void testOutdegree()
+	{
+
+	}
+
 }
